354c437c27aa012743176b013480c838776c3ba1
farhanmasudmm-crypto <farhanmasud.mm@gmail.com>
2026-04-09 00:49:00 +0600

    Add Vehicle class with wheel count and max speed

---
+++ Vehicle.java
@@ -0,0 +1,14 @@
+
+public class Vehicle {
+    private final int wheelCount;
+    private final int maxSpeed;
+
+    public Vehicle(int wheelCount, int maxSpeed) {
+        this.wheelCount = wheelCount;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getWheelCount() { return wheelCount; }
+    public int getMaxSpeed() { return maxSpeed; }
+}
+
